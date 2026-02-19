561bdb81e2fc09a11feae594a45d5839ec653e2d
AmitParihar17 <amitparihar1208@gmail.com>
2026-02-19 21:43:10 +0530

    add new files

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,12 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 py-3 px-6 rounded-sm m-4"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
-}
+ "use client"
+ 
+ const page = () => {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default page
+ 

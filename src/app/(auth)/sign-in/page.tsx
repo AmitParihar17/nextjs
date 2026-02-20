95cd381039b73b0873a5e25332495ef30c36b590
AmitParihar17 <amitparihar1208@gmail.com>
2026-02-20 19:22:32 +0530

    add routes

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,11 @@
- "use client"
- 
- const page = () => {
-   return (
-     <div>
-       
-     </div>
-   )
- }
- 
- export default page
- 
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      page
+    </div>
+  )
+}
+
+export default page
